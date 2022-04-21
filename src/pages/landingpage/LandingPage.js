705eab8fe6f2d26ca28c1bf0fbf01c861b18b55a
Smiley <jcsmileyjr@gmail.com>
2022-04-20 20:21:11 -0500

    Initial Landing Page setup

---
+++ src/pages/landingpage/LandingPage.js
@@ -0,0 +1,11 @@
+import "./landingPage.css";
+
+const landingPage = () => {
+    return (
+        <div>
+            <h1 className="landingPage--title__style">Let's Play <br/>Tic Tac Toe</h1>
+        </div>
+    );
+}
+
+export default landingPage;
