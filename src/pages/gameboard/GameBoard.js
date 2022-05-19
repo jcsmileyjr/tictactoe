396abaf376e9177a6b9b5109ffcb2ecded501839
Smiley <jcsmileyjr@gmail.com>
2022-05-18 21:06:11 -0500

    Gameboard information board initial styling

--- src/pages/gameboard/GameBoard.js
+++ src/pages/gameboard/GameBoard.js
@@ -6,6 +6,26 @@ const GameBoard = ({userIcon}) => {
     return(
         <main className="page__container">
             <h1 className=" gameBoard--title__style darkGrayColor">Tic Tac Toe</h1>
+            <section className="information--container">
+                <div className="information__wins--container">
+                    <p>Wins</p>
+                    <p>5</p>
+                </div>
+                <div className="information__ties--container">
+                    <p>Ties</p>
+                    <p>3</p>
+                </div>
+                <div className="information__losses--container">
+                    <p>Losses</p>
+                    <p>2</p>
+                </div>
+                <div className="information__restart--container">
+                    <p>Restart</p>
+                    <button>
+                        <div className="oIcon--style gameboard__restart--style"></div>
+                    </button>
+                </div>
+            </section>
         </main>
     );
 }
